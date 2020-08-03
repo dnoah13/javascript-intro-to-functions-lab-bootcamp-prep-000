c057ebd871e47e27ec28b22e3507dcc608120346
Daniel Wills <daniel.wills83@gmail.com>
2020-08-03 03:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === 'string') {
+  if (string.toLowerCase() === lowerCase) {
     return "I can't hear you!"
   }
 }
